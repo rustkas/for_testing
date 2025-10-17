814ba7b085f43fd1f94a5097f1c9e66299c3c5d1
Anatolii Kosorukov <java1cprog@yandex.ru>
2025-10-17 10:41:14 +0700

    Add start1777 command

---
+++ commands/start1777.js
@@ -0,0 +1,16 @@
+/*CMD
+id: 69613027
+command: /start1777
+help: 
+need_reply: false
+auto_retry_time: 
+folder: 
+answer: 
+keyboard: 
+aliases: 
+group: 
+updated_at: 2025-10-15T03:40:39.143Z
+folder_id: 
+CMD*/
+
+var a = 19;
