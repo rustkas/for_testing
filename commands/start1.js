b9896cafd601aad1b5beaded7186fa200300e0a3
Anatolii Kosorukov <java1cprog@yandex.ru>
2025-10-17 10:41:33 +0700

    Add start1 command

---
+++ commands/start1.js
@@ -0,0 +1,16 @@
+/*CMD
+id: 69535046
+command: start1
+help: 
+need_reply: false
+auto_retry_time: 
+folder: 
+answer: 
+keyboard: 
+aliases: 
+group: 
+updated_at: 2025-10-10T07:41:35.207Z
+folder_id: 
+CMD*/
+
+// Empty command
