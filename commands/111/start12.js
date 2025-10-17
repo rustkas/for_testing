a0c4321c5a262f242c6431893cf3ee8afdbf4096
Anatolii Kosorukov <java1cprog@yandex.ru>
2025-10-17 10:41:47 +0700

    Add start12 command in folder

---
+++ commands/111/start12.js
@@ -0,0 +1,16 @@
+/*CMD
+id: 69535047
+command: start12
+help: 
+need_reply: false
+auto_retry_time: 
+folder: 111
+answer: 
+keyboard: 
+aliases: 
+group: 
+updated_at: 2025-10-10T11:18:29.082Z
+folder_id: 7648164
+CMD*/
+
+// Empty command
