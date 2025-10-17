c1206d439ee196a6775746c5fa09020b4782c91a
Anatolii Kosorukov <java1cprog@yandex.ru>
2025-10-17 10:40:56 +0700

    Add start command

---
+++ commands/start.js
@@ -0,0 +1,17 @@
+/*CMD
+id: 69535045
+command: /start
+help: Help. It is Start command
+need_reply: false
+auto_retry_time: 
+folder: 
+answer: Help. It is Start command
+keyboard: 
+aliases: 
+group: 
+updated_at: 2025-10-15T03:33:21.539Z
+folder_id: 
+CMD*/
+
+var a = 19;
+var b = 19;
